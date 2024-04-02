4daa6e77b11e1a2dfda9c34aaea0385b4c247567
Pradeep Edirisinghe <pradeep.edirisinghe@outlook.com>
2024-04-02 01:15:34 -0700

    added aws_route_table_association resources as dpp-rta-public-subnet-02

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -57,15 +57,24 @@ resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
   route_table_id = aws_route_table.dpp-public-rt.id
 }
 
+resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
+  subnet_id      = aws_subnet.dpp-public-subnet-02.id
+  route_table_id = aws_route_table.dpp-public-rt.id
+}
+
 resource "aws_instance" "demo-server" {
     ami = "ami-0eb5115914ccc4bc2"
     instance_type = "t2.micro"
     key_name = "dpp"
+    //security_groups = [ demo-sg ]
+    vpc_security_group_ids = [aws_security_group.demo-SG.id]
+    subnet_id = aws_subnet.dpp-public-subnet-01.id
 }
 
 resource "aws_security_group" "demo-SG" {
   name        = "demo-sg"
   description = "SSH Access"
+  vpc_id = aws_vpc.dpp-vpc.id
   
   ingress {
     description      = "ssh access"
