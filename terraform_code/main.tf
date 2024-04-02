09b0c169888958500b0def0fd96d7d24b2c7127a
Pradeep Edirisinghe <pradeep.edirisinghe@outlook.com>
2024-04-01 22:59:23 -0700

    added demo-SG resources in aws_security_group

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -7,3 +7,28 @@ resource "aws_instance" "demo-server" {
     instance_type = "t2.micro"
     key_name = "dpp"
 }
+
+resource "aws_security_group" "demo-SG" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
