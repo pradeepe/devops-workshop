03f403c9cd18aaf795cf623c7a88f8069d839309
Pradeep Edirisinghe <pradeep.edirisinghe@outlook.com>
2024-07-20 03:18:00 -0700

    added Security group resource to the Vi-EC2.rt file

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -6,4 +6,35 @@ resource "aws_instance" "demo-server" {
     ami = "ami-0b72821e2f351e396"
     instance_type = "t2.micro"
     key_name = "dpp"
+    security_groups = [ "demo-sg" ]
+
+    tags = {
+      Name = "Demo-instance"
+
+  }
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
 }
