70249046569c672dbab70a4e8747b8cad20fd02c
Pradeep Edirisinghe <pradeep.edirisinghe@outlook.com>
2025-02-17 02:52:28 -0800

    Adding two aws_subnet

--- main.tf
+++ main.tf
@@ -8,3 +8,23 @@ resource "aws_vpc" "dpp-vpc" {
     Name = "dpp-vpc"
   }
 }
+
+resource "aws_subnet" "dpp-pub-subnet-01" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "dpp-public-subent-01"
+  }
+}
+
+resource "aws_subnet" "dpp-pub-subnet-02" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "dpp-public-subent-02"
+  }
+}
