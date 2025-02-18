39b9a2147010ff55522a9a213b4878efea6cc371
Pradeep Edirisinghe <pradeep.edirisinghe@outlook.com>
2025-02-17 22:19:41 -0800

    Added Jenkins port opend for 8080

--- main.tf
+++ main.tf
@@ -160,6 +160,14 @@ resource "aws_security_group" "jenkins-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+    ingress {
+    description = "Jenkins port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
  
   egress {
     from_port   = 0
